2e06bfe0982e63e29e4632834498c3736d6b1036
Joel Paul <joel.v.paul@outlook.com>
2021-11-29 15:37:04 +1100

    Rearrange achievements

--- minecraft/net/minecraft/src/example/ExampleAchievements.java
+++ minecraft/net/minecraft/src/example/ExampleAchievements.java
@@ -44,23 +44,23 @@ private static ExampleAchievements instance;
 		/* 
 		 * DO NOT set any achievement to independent (i.e. don't call .setIndependent())
 		 * as this may cause issues for some achievement triggers. Setting the parent achievement
-		 * to null works completely fine so there shouldn't be a reason to use it anyway.
+		 * to (Achievement)null works completely fine so there shouldn't be a reason to use it anyway.
 		 */
 		
 		AchievementTab tabSimple = new AchievementTab("simple").setIcon(FCBetterThanWolves.fcCompanionCube);
-		chiselWood = (new Achievement("chiselWood", 0, 1, FCBetterThanWolves.fcItemChiselWood, (Achievement)null)).registerAchievement(tabSimple);
-		chiselStone = (new Achievement("chiselStone", 0, -1, FCBetterThanWolves.fcItemChiselStone, (Achievement)null)).registerAchievement(tabSimple);
-		killPig = (new Achievement("killPig", 1, 0, Item.porkRaw, chiselWood, chiselStone)).registerAchievement(tabSimple);
-		cookPork = (new Achievement("cookPork", 2, 0, Item.porkCooked, killPig)).registerAchievement(tabSimple);
-		cookIron = (new Achievement("cookIron", 3, 0, FCBetterThanWolves.fcItemNuggetIron, cookPork)).registerAchievement(tabSimple);
+		mineBark = (new Achievement("mineBark", 0, -1, FCBetterThanWolves.fcItemBark, (Achievement)null)).registerAchievement(tabSimple);
+		mineStick = (new Achievement("mineStick", 0, 1, Item.stick, (Achievement)null)).registerAchievement(tabSimple);
+		chiselWood = (new Achievement("chiselWood", 1, 0, FCBetterThanWolves.fcItemChiselWood, mineBark, mineStick)).registerAchievement(tabSimple);
+		chiselStone = (new Achievement("chiselStone", 2, 0, FCBetterThanWolves.fcItemChiselStone, chiselWood)).registerAchievement(tabSimple);
+		killPig = (new Achievement("killPig", 2, -1, Item.porkRaw, chiselStone)).registerAchievement(tabSimple);
+		cookPork = (new Achievement("cookPork", 3, -1, Item.porkCooked, killPig)).registerAchievement(tabSimple);
+		cookIron = (new Achievement("cookIron", 3, 0, FCBetterThanWolves.fcItemNuggetIron, chiselStone)).registerAchievement(tabSimple);
+		neuterCreeper = (new Achievement("neuterCreeper", 4, 1, FCBetterThanWolves.fcItemCreeperOysters, cookIron)).registerAchievement(tabSimple);
 		System.out.println("Simple Tab: " + tabSimple.size() + " achievements");
 		
 		AchievementTab tabCustom = new CustomAchievementTab("custom").setIcon(FCBetterThanWolves.fcItemWaterWheel);
-		mineBark = (new Achievement("mineBark", 0, 0, FCBetterThanWolves.fcItemBark, (Achievement)null)).registerAchievement(tabCustom);
-		mineStick = (new Achievement("mineStick", 1, 0, Item.stick, mineBark)).registerAchievement(tabCustom);
-		neuterCreeper = (new Achievement("neuterCreeper", 2, 0, FCBetterThanWolves.fcItemCreeperOysters, mineStick)).registerAchievement(tabCustom);
-		netherPortal = (new Achievement("netherPortal", 3, 0, Block.obsidian, mineStick)).registerAchievement(tabCustom);
-		firePotion = (new Achievement("firePotion", 4, 0, Item.potion, netherPortal)).setSpecial().registerAchievement(tabCustom);
+		netherPortal = (new Achievement("netherPortal", 0, 0, Block.obsidian, cookIron)).registerAchievement(tabCustom);
+		firePotion = (new Achievement("firePotion", 0, 1, Item.potion, netherPortal)).setSpecial().registerAchievement(tabCustom);
 		System.out.println("Custom Tab: " + tabCustom.size() + " achievements");
 		
 		EventDispatcher.register(new ExampleEventHandler());
