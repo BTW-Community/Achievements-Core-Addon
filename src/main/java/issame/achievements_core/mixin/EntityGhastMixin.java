4f7583daae1d41bb9461fdd7186dec90506a0a50
Joel Paul <joel.v.paul@outlook.com>
2023-09-25 12:23:49 +1000

    Make EntityGhastMixin abstract

--- src/main/java/issame/achievements_core/mixin/EntityGhastMixin.java
+++ src/main/java/issame/achievements_core/mixin/EntityGhastMixin.java
@@ -7,21 +7,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EntityGhast.class)
-public class EntityGhastMixin extends EntityFlying {
+public abstract class EntityGhastMixin extends EntityFlying {
     public EntityGhastMixin(World par1World) {
         super(par1World);
     }
 
-    @Redirect(method = "Lnet/minecraft/src/EntityGhast;attackEntityFrom(Lnet/minecraft/src/DamageSource;I)Z",
+    @Redirect(method = "attackEntityFrom(Lnet/minecraft/src/DamageSource;I)Z",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/src/EntityPlayer;triggerAchievement(Lnet/minecraft/src/StatBase;)V"))
     private void killedByReturn(EntityPlayer player, StatBase par1StatBase) {
         EventDispatcher.onReturnedToGhast(player);
     }
-
-    //Had to be implemented
-    @Override
-    public int getMaxHealth() {
-        return 10;
-    }
-
 }
